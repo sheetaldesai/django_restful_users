6ce0b222c2c3ca7faf3ef2a7933e41e60e1d8e61
sheetaldesai <sheetaldesai1@gmail.com>
2017-10-12 22:05:12 -0700

    modified files

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 app_name = 'users'
 urlpatterns = [
-    url(r'^$', views.index, name="home",kwargs={'id':0} ),
-    url(r'^addEdit/$', views.addEdit, name="addEdit"),
-    url(r'^/(?P<userId>[0-9]+)/edit/$', views.edit, name='edit'),
+    url(r'^$', views.index, name="home"),
+    url(r'^addEdit/(?P<userId>[0-9]+)/$', views.addEdit, name="addEdit"),
+    url(r'^(?P<userId>[0-9]+)/edit/$', views.edit, name='edit'),
     url(r'^/(?P<userId>[0-9]+)/delete/$', views.delete, name='delete')
     # url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     # url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote')
--- apps/users/views.py
+++ apps/users/views.py
@@ -10,26 +10,35 @@ from .forms import UserForm
 from django.utils import timezone
 
 
-def index (request, id) :
-    print "id: ",id
-    if (id) :
-        user = User.objects.get(pk=id)
-        print "user: ", user
-        form = UserForm(user)
-        addEdit = "Update"
-    else:
-        form = UserForm()
-        addEdit = "Add"
-    context = {'users': Users.objects.all(),
-                'form' : form,
-                'addEdit' : addEdit}
+def index (request) :
+    
+    
+    context = {'users': Users.objects.all()
+              }
     print context
     return render(request, 'users/index.html', context)
 
 def edit (request, userId) :
     print "in edit, id: ",userId
-    return redirect(reverse('users:home'),kwargs={'id': userId })
+    
+    if int(userId) != 0:
+        print "edit mode"
+        user = Users.objects.get(pk=userId)
+        print "user: ", user
+        data = {'first_name': user.first_name, 'last_name': user.last_name, 'email_address': user.email_address}
+        form = UserForm(initial = data)
+        addEdit = "Update"
+    else:
+        print "add mode"
+        form = UserForm()
+        addEdit = "Add"
+    
+    context = {'form' : form,
+                'addEdit': addEdit,
+                'id': userId}
 
+    return render(request, 'users/addEdit.html', context)
+ 
 def delete (request, userId) :
     print "in delete, id: ", userId
     user = Users.objects.get(pk=userId)
@@ -37,12 +46,21 @@ def delete (request, userId) :
     user.delete()
     return redirect(reverse('users:home'),kwargs={'id': 0 })
     
-def addEdit(request) :
+def addEdit(request, userId) :
     
+
     if request.method == "POST" :
         print "in addEdit, request.POST: ", request.POST
         if ("Add" in request.POST):
             # Add new record
             Users.objects.create(first_name = request.POST["first_name"], last_name = request.POST["last_name"], email_address = request.POST["email_address"], created_at = timezone.now())
+        else :
+            user = Users.objects.get(pk=userId)
+            print user
+            user.first_name = request.POST["first_name"]
+            user.last_name = request.POST["last_name"]
+            user.email_address = request.POST["email_address"]
+            user.updated_at = timezone.now()
+            user.save()
 
-    return redirect(reverse('users:home'),kwargs={'id': 0 })
+    return redirect(reverse('users:home'))
